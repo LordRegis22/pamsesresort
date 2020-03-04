71cc7acbfb8807f0c1198c2873118897df4b87c9
Michael O <michaelohalloran@Michaels-MBP.home>
2020-03-03 22:47:55 -0800

    Added services and title components

---
+++ src/components/Services.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Title from "../components/Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaPaw } from "react-icons/fa";
+
+export default function Services() {
+  const [services, setServices] = useState([
+    {
+      icon: <FaCocktail />,
+      title: "Free Cocktails",
+      info:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab neque id iusto illo laudantium soluta."
+    },
+    {
+      icon: <FaHiking />,
+      title: "Hike your face off!",
+      info:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab neque id iusto illo laudantium soluta."
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: "Sit on a bus!",
+      info:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab neque id iusto illo laudantium soluta."
+    },
+    {
+      icon: <FaPaw />,
+      title: "Meet Pamses",
+      info:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab neque id iusto illo laudantium soluta."
+    }
+  ]);
+  return (
+    <section className="services">
+      <Title title="Services"></Title>
+      <div className="services-center">
+        {services.map((service, index) => {
+          return (
+            <article key={index} className="service">
+              <span>{service.icon}</span>
+              <h6>{service.title}</h6>
+              <p>{service.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
